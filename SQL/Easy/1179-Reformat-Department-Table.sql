fa3cc46739cdbb5ea4030493c1b4e02119afa3fc
hyunjung <space90220@gmail.com>
2025-07-12 01:08:58 +0900

    2025.07.12 - 1179

---
+++ SQL/Easy/1179-Reformat-Department-Table.sql
@@ -0,0 +1,42 @@
+-- 출처 : https://leetcode.com/problems/reformat-department-table/description
+
+-- case1
+select id
+    , sum(case when month = 'Jan' then revenue end) as Jan_Revenue
+    , sum(case when month = 'Feb' then revenue end) as Feb_Revenue
+    , sum(case when month = 'Mar' then revenue end) as Mar_Revenue
+    , sum(case when month = 'Apr' then revenue end) as Apr_Revenue
+    , sum(case when month = 'May' then revenue end) as May_Revenue
+    , sum(case when month = 'Jun' then revenue end) as Jun_Revenue
+    , sum(case when month = 'Jul' then revenue end) as Jul_Revenue
+    , sum(case when month = 'Aug' then revenue end) as Aug_Revenue
+    , sum(case when month = 'Sep' then revenue end) as Sep_Revenue
+    , sum(case when month = 'Oct' then revenue end) as Oct_Revenue
+    , sum(case when month = 'Nov' then revenue end) as Nov_Revenue
+    , sum(case when month = 'Dec' then revenue end) as Dec_Revenue
+from Department
+group by id;
+
+-- case2
+select *
+from (
+    select id as "id", revenue, month
+    from Department
+)
+pivot (
+    sum(revenue)
+    for month in (
+        'Jan' as Jan_Revenue,
+        'Feb' as Feb_Revenue,
+        'Mar' as Mar_Revenue,
+        'Apr' as Apr_Revenue,
+        'May' as May_Revenue,
+        'Jun' as Jun_Revenue,
+        'Jul' as Jul_Revenue,
+        'Aug' as Aug_Revenue,
+        'Sep' as Sep_Revenue,
+        'Oct' as Oct_Revenue,
+        'Nov' as Nov_Revenue,
+        'Dec' as Dec_Revenue
+    )
+);
--- SQL/Hard/262-Trips-and-Users.sql
+++ SQL/Hard/262-Trips-and-Users.sql
@@ -1,5 +1,6 @@
 -- 출처 : https://leetcode.com/problems/trips-and-users/description
 
+-- case1
 select t.request_at as "Day"
       , round(sum(case when t.status like 'cancelled%' then 1 else 0 end) / count(*), 2) as "Cancellation Rate"
 from Trips t
@@ -8,6 +9,16 @@ on u1.users_id = t.client_id
 and u1.banned = 'No'
 inner join Users u2
 on u2.users_id = t.driver_id
-and u1.banned = 'No'
+and u2.banned = 'No'
+where request_at between '2013-10-01' and '2013-10-03'
 group by t.request_at
-having count(*) > 1
+having count(*) > 0;
+
+-- case2
+select t.request_at as "Day"
+      , round(sum(case when t.status in ('cancelled_by_driver', 'cancelled_by_client') then 1 else 0 end) / count(*), 2) as "Cancellation Rate"
+from Trips t
+where t.client_id in (select users_id from Users where banned = 'No' and role = 'client')
+and t.driver_id in (select users_id from Users where banned = 'No' and role = 'driver')
+and t.request_at between '2013-10-01' and '2013-10-03'
+group by t.request_at
